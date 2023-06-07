1670e2949f7dab647c96abeacc0ae8571f4d3293
Kevin Carter <console.logkevin@gmail.com>
2023-06-07 15:57:39 -0400

    add ng router set router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 /* Components */
-import {  } 
+import { HomeComponent } from '../app/components/home/home.component';
+import { DestinationsComponent } from '../app/components/destinations/destinations.component';
+import { CrewComponent } from '../app/components/crew/crew.component';
+import { TechComponent } from '../app/components/tech/tech.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'destinations', component: DestinationsComponent},
+  {path: 'crew', component: CrewComponent},
+  {path: 'tech', component: TechComponent},
+  {path: '**',  component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
