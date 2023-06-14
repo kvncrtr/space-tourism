47f0e6a039d39ae31a282bede2d0be9402fb93ad
Kevin Carter <console.logkevin@gmail.com>
2023-06-14 08:31:04 -0400

    add ui goodies to all pages making them minimum style requirements

--- src/app/space-data.ts
+++ src/app/space-data.ts
@@ -88,7 +88,7 @@ export const SPACE: Space = {
             "portrait": "./assets/technology/image-launch-vehicle-portrait.jpg",
             "landscape": "./assets/technology/image-launch-vehicle-landscape.jpg"
          },
-         "description": "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!"
+         "description": "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch!"
       },
       {
          "name": "Spaceport",
